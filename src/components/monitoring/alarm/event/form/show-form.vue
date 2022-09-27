7e0026897e098921f2cc6b58fc835202eeb7946d
合肥.田婷 <371129146@qq.com>
2022-09-27 15:01:20 +0800

    告警事件新增补充说明

--- src/components/monitoring/alarm/event/form/show-form.vue
+++ src/components/monitoring/alarm/event/form/show-form.vue
@@ -70,13 +70,66 @@
           <div class="content" style="overflow: hidden">
             <chart-line :detail="detail" :filters="filters" :isRecord="true" chartWidth="100%"/>
           </div>
+
         </div>
+        <!--  补充说明   -->
+        <div class="form-header-part">
+              <div class="header">
+                  <div class="sign f-dib"></div>
+                  <h3 :class="{active: pageSets[2].key === currentTab.key}" class="tit f-dib index-tit">
+                      {{pageSets[2].name}}</h3>
+              </div>
+              <div class="content" style="overflow: hidden">
+                      <el-row>
+                          <el-table
+                              :data="tableData"
+                              border
+                              max-height="500px"
+                              style="width: 100%">
+                              <el-table-column
+                                  type="index"
+                                  label="序号"
+                                  width="50"/>
+                              <el-table-column
+                                  prop="explainContent"
+                                  label="内容"/>
+                              <el-table-column
+                                  prop="creatorName"
+                                  label="操作人"
+                                  width="150"/>
+                              <el-table-column
+                                  prop="createTime"
+                                  label="操作时间"
+                                  width="150">
+                                  <template slot-scope="{row}">
+                                      <span>{{formatTime(row.createTime)}}</span>
+                                  </template>
+                              </el-table-column>
+                          </el-table>
+                      </el-row>
+                      <el-row style="text-align: end;margin: 10px 0px">
+                          <el-button type="primary" @click="onAddExplain">新增说明</el-button>
+                      </el-row>
+                      <div v-if="isShowExplain">
+                          <el-form ref="form" :model="form" label-width="0" :rules="formRules">
+                              <el-form-item label="" prop="explainContent">
+                                  <el-input type="textarea" :autosize="{ minRows: 3, maxRows: 4}"  placeholder="请填写补充说明"  maxlength="199"
+                                            show-word-limit v-model="form.explainContent"></el-input>
+                              </el-form-item>
+                              <el-form-item style="text-align: end;">
+                                  <el-button type="primary" @click="onSubmitExplain">保存</el-button>
+                                  <el-button @click="resetExplain">取消</el-button>
+                              </el-form-item>
+                          </el-form>
+                      </div>
+                  </div>
+          </div>
       </div>
     </template>
   </dialog-template>
 </template>
 <script>
-    import {WarnRecord} from '@/resources';
+    import {WarnRecord,warnExplain} from '@/resources';
     import utils from '@/tools/utils';
     import ChartLine from '@/components/monitoring/temp-new/chart-line-new';
     import AlarmMixin from '@/mixins/alarmMixin';
@@ -96,7 +149,8 @@
                 loading: false,
                 pageSets: [
                     {name: '详细信息', key: 0},
-                    {name: '历史数据', key: 0}
+                    {name: '历史数据', key: 0},
+                    {name: '补充说明', key: 0},
                 ],
                 currentTab: {},
                 tempList: [],
@@ -108,7 +162,16 @@
                     devCode: '',
                     valType: ['1']
                 },
-                parent: this.$parent.$parent
+                parent: this.$parent.$parent,
+                isShowExplain:false,//是否显示补充说明
+                tableData: [],
+                form:{},
+                formRules: {
+                    explainContent: [
+                        {required: true, message: '请输入补充说明', trigger: 'blur'},
+                        { max: 199, message: '补充说明字数限制199', trigger: 'blur'}
+                    ],
+                }
             };
         },
         watch: {
@@ -130,11 +193,13 @@
             },
             queryDetail() {
                 this.loading = true;
+                this.resetExplain();
                 WarnRecord.get(this.formItem.id).then(res => {
                     this.detail = res.data;
                     this.loading = false;
                     this.$nextTick(() => {
                         this.queryTempData();
+                        this.queryTableData();
                     });
                 });
             },
@@ -157,6 +222,40 @@
                     devCode: devCode,
                     valType: getValType(warnTypes)
                 };
+            },
+            queryTableData(){
+                warnExplain.getWarnExplain(this.formItem.id).then(res => {
+                  this.tableData=res.data.resultList||[];
+                });
+            },
+            onAddExplain(){
+             this.isShowExplain=true;
+             this.form={
+                 ccsWarnRecordId:this.formItem.id,
+                 explainContent:'',
+             };
+            },
+            onSubmitExplain(){
+                this.$refs.form.validate((valid, obj) => {
+                    if(!valid) return
+                    this.$httpRequestOpera(warnExplain.addWarnExplain(this.form), {
+                        successTitle: '添加成功',
+                        errorTitle: '添加失败',
+                        success: res => {
+                            this.queryTableData();
+                            this.resetExplain();
+                        },
+                        error: () => {
+                        }
+                    });
+                    });
+            },
+            resetExplain(){
+                this.form={
+                    ccsWarnRecordId:this.formItem.id,
+                    explainContent:'',
+                };
+                this.isShowExplain=false;
             }
         }
     };
