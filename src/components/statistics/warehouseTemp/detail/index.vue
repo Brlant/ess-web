885ec30168ab6ae99f1eb58a83917934ea565478
合肥.田婷 <371129146@qq.com>
2022-11-04 09:38:34 +0800

    库内温度巡检功-详情优化

--- src/components/statistics/warehouseTemp/detail/index.vue
+++ src/components/statistics/warehouseTemp/detail/index.vue
@@ -6,34 +6,32 @@
         <div>
             <h1>统计</h1>
             <el-row style="margin: 20px 0">
-                <span>点位总数：{{infoTotal.count}} </span>
-                <span style="margin-left: 20px">缺失点位数量：{{infoTotal.defectCount}}  </span>
-                <span style="margin-left: 20px">累计缺失点位数量：{{infoTotal.defectSum}} </span>
+                <span>点位总数：{{ infoTotal.count }} </span>
+                <span style="margin-left: 20px">缺失点位数量：{{ infoTotal.defectCount }}  </span>
+                <span style="margin-left: 20px">累计缺失点位数量：{{ infoTotal.defectSum }} </span>
             </el-row>
-            <el-table :data="tableAllData" v-loading="tableAllLoad" max-height="400px"  border style="width: 100%">
+            <el-table :data="tableAllData" v-loading="tableAllLoad" max-height="400px" border style="width: 100%">
                 <el-table-column type="index" label="序号" width="60"/>
                 <el-table-column prop="pointName" label="点位名称" width="150"/>
-                <el-table-column  label="设备名称/编码" width="150">
-                    <template slot-scope="{row}" class="dev-div">
-                        <div class="dev-div">
-                            <span>{{row.devName}}</span>
-                            <span>{{row.devCode}}</span>
-                        </div>
+                <el-table-column label="设备名称/编码" width="300">
+                    <template slot-scope="{row}">
+                        <div>{{ row.devName }}</div>
+                        <div>{{ row.devCode }}</div>
                     </template>
                 </el-table-column>
                 <el-table-column label="位置">
                     <template slot-scope="{row}">
-                        <span>{{row.logisticsCode}}</span>
-                        <span v-if="row.warehouseCode">{{'-'+row.warehouseCode}}</span>
-                        <span v-if="row.areaCode">{{'-'+row.areaCode}}</span>
+                        <span>{{ row.logisticsCode }}</span>
+                        <span v-if="row.warehouseCode">{{ '-' + row.warehouseCode }}</span>
+                        <span v-if="row.areaCode">{{ '-' + row.areaCode }}</span>
                     </template>
                 </el-table-column>
-                <el-table-column  label="监控状态" width="120">
-                    <template slot-scope="{row}" >
-                            <span>{{row.monitorStatus=='1'?'激活':row.monitorStatus=='0'?'未激活':''}}</span>
+                <el-table-column label="监控状态" width="100">
+                    <template slot-scope="{row}">
+                        <span>{{ row.monitorStatus == '1' ? '激活' : row.monitorStatus == '0' ? '未激活' : '' }}</span>
                     </template>
                 </el-table-column>
-                <el-table-column prop="loseCount" label="累计缺失点" width="120"/>
+                <el-table-column prop="loseCount" label="累计缺失点" width="100"/>
             </el-table>
             <div class="text-center" v-show="(tableAllData.length || pagerTable.currentPage !== 1) && !tableAllLoad">
                 <el-pagination :current-page="pagerTable.currentPage" :page-size="pagerTable.pageSize"
@@ -46,27 +44,25 @@
         </div>
         <div>
             <h1>明细</h1>
-            <el-table :data="tableData" v-loading="tableLoad" border  max-height="500px" style="width: 100%">
+            <el-table :data="tableData" v-loading="tableLoad" border max-height="500px" style="width: 100%">
                 <el-table-column type="index" label="序号" width="60"/>
                 <el-table-column prop="pointName" label="点位名称" width="150"/>
-                <el-table-column  label="设备名称/编码" width="150">
-                    <template slot-scope="{row}" >
-                        <div class="dev-div">
-                            <span>{{row.devName}}</span>
-                            <span>{{row.devCode}}</span>
-                        </div>
+                <el-table-column label="设备名称/编码" width="300">
+                    <template slot-scope="{row}">
+                        <div>{{ row.devName }}</div>
+                        <div>{{ row.devCode }}</div>
                     </template>
                 </el-table-column>
                 <el-table-column label="位置">
                     <template slot-scope="{row}">
-                        <span>{{row.logisticsCode}}</span>
-                        <span v-if="row.warehouseCode">{{'-'+row.warehouseCode}}</span>
-                        <span v-if="row.areaCode">{{'-'+row.areaCode}}</span>
+                        <span>{{ row.logisticsCode }}</span>
+                        <span v-if="row.warehouseCode">{{ '-' + row.warehouseCode }}</span>
+                        <span v-if="row.areaCode">{{ '-' + row.areaCode }}</span>
                     </template>
                 </el-table-column>
-                <el-table-column  label="监控状态" width="120">
-                    <template slot-scope="{row}" >
-                        <span>{{row.monitorStatus=='1'?'激活':row.monitorStatus=='0'?'未激活':''}}</span>
+                <el-table-column label="监控状态" width="100">
+                    <template slot-scope="{row}">
+                        <span>{{ row.monitorStatus == '1' ? '激活' : row.monitorStatus == '0' ? '未激活' : '' }}</span>
                     </template>
                 </el-table-column>
                 <el-table-column prop="loseTime" label="缺失时间" width="150"/>
@@ -87,15 +83,16 @@
 <script>
 import CommonMixin from '@/mixins/commonMixin';
 import {WarehouseTemp} from '@/resources';
+
 export default {
     name: "index",
     mixins: [CommonMixin],
     data() {
         return {
-            infoTotal:{
-                defectCount:null,
-                defectSum:null,
-                count:null,
+            infoTotal: {
+                defectCount: null,
+                defectSum: null,
+                count: null,
             },
             tableAllData: [],
             tableAllLoad: false,
@@ -106,57 +103,61 @@ export default {
                 currentPage: 1,
                 count: 0,
             },
-            taskId:''
+            taskId: ''
         }
     },
-    mounted(){
-        this.taskId=this.$route.query.id;
-        if(this.taskId==undefined) return
+    mounted() {
+        this.taskId = this.$route.query.id;
+        if (this.taskId == undefined) return
         this.queryTable();
         this.queryList();
     },
     methods: {
-        queryTable(){
-            let param={
-                taskId:this.taskId,
-                pageNo:this.pagerTable.currentPage,
-                pageSize:this.pagerTable.pageSize
+        queryTable() {
+            let param = {
+                taskId: this.taskId,
+                pageNo: this.pagerTable.currentPage,
+                pageSize: this.pagerTable.pageSize
             }
-            this.tableAllLoad=true
-            WarehouseTemp.getResultTask(param).then(res=>{
-                this.tableAllData=res.data.currentList;
-                this.infoTotal.defectCount=res.data.count
-                this.pagerTable.count=this.tableAllData.length;
-                WarehouseTemp.getTaskCount(this.taskId).then(res1=> {
-                    this.infoTotal.count=res1.data
+            this.tableAllLoad = true
+            WarehouseTemp.getResultTask(param).then(res => {
+                this.tableAllData = res.data.currentList;
+                this.infoTotal.defectCount = res.data.count
+                this.pagerTable.count = this.tableAllData.length;
+                WarehouseTemp.getTaskCount(this.taskId).then(res1 => {
+                    this.infoTotal.count = res1.data
                 })
-                this.tableAllLoad=false;
-            }).catch(()=> {this.tableAllLoad=false;})
+                this.tableAllLoad = false;
+            }).catch(() => {
+                this.tableAllLoad = false;
+            })
         },
-        queryList(){
-            let param={
-                taskId:this.taskId,
-                pageNo:this.pager.currentPage,
-                pageSize:this.pager.pageSize
+        queryList() {
+            let param = {
+                taskId: this.taskId,
+                pageNo: this.pager.currentPage,
+                pageSize: this.pager.pageSize
             }
-            this.tableLoad=true;
-            WarehouseTemp.getResultDetailTask(param).then(res=>{
-                this.tableData=res.data.currentList;
-                this.infoTotal.defectSum=res.data.count
-                this.pager.count=this.tableData.length;
-                this.tableLoad=false;
-            }).catch(()=> { this.tableLoad=false;})
+            this.tableLoad = true;
+            WarehouseTemp.getResultDetailTask(param).then(res => {
+                this.tableData = res.data.currentList;
+                this.infoTotal.defectSum = res.data.count
+                this.pager.count = this.tableData.length;
+                this.tableLoad = false;
+            }).catch(() => {
+                this.tableLoad = false;
+            })
         },
         handleReturn() {
             this.$router.push('/statistics/warehouseTemp')
         },
         handleTableCurrentChange(val) {
-            this.pagerTable.currentPage=val;
+            this.pagerTable.currentPage = val;
             this.queryTable();
 
         },
         handleTableSizeChange(val) {
-            this.pagerTable.pageSize=val;
+            this.pagerTable.pageSize = val;
             window.localStorage.setItem('currentTotalPageSize', val);
             this.queryTable();
         },
@@ -174,11 +175,8 @@ export default {
 </script>
 
 <style scoped>
-.text-center{
+.text-center {
     margin-top: 10px;
 }
-.dev-div{
-    display: flex;
-    flex-wrap: wrap
-}
+
 </style>
